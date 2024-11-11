37bc8bcf0be010011334663afd58e7fbb4547e76
Richard Fontein <32132657+rifont@users.noreply.github.com>
2024-11-11 02:02:45 +0100

    Update packages/framework/src/validators/validator.test.ts

--- packages/framework/src/validators/validator.test.ts
+++ packages/framework/src/validators/validator.test.ts
@@ -1,9 +1,8 @@
 import { describe, it, expect } from 'vitest';
 import { z } from 'zod';
 import { validateData, transformSchema } from './base.validator';
-import type { Schema, ZodSchema, JsonSchema } from '../types/schema.types';
+import type { Schema, ZodSchema, JsonSchema, ClassValidatorSchema } from '../types/schema.types';
 import { SimpleStringSchema, NestedSchema, SimpleStringAndNumberSchema } from './fixures/class-validator.fixtures';
-import { ClassValidatorSchema } from '../types/schema.types/class.schema.types';
 
 const schemas = ['zod', 'class', 'json'] as const;
 
